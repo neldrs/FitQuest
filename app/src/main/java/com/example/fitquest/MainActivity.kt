1a1e6e6170ec3aaabbfacb89dafd497f39867372
ElianRenteria <elianrenteriadevelopment@gmail.com>
2023-11-19 23:41:04 -0800

    notifications working

--- app/src/main/java/com/example/fitquest/MainActivity.kt
+++ app/src/main/java/com/example/fitquest/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.fitquest
 
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,6 +25,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,10 +33,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    private val REQUEST_CODE = 123
+
+    @SuppressLint("ScheduleExactAlarm")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         auth = Firebase.auth
+
+        // Check if the permission is not granted
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val exactAlarmPermission = "android.permission.SCHEDULE_EXACT_ALARM"
+            if (checkSelfPermission(exactAlarmPermission) != PackageManager.PERMISSION_GRANTED) {
+                // Request the permission
+                requestPermissions(arrayOf(exactAlarmPermission), REQUEST_CODE)
+            }
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -42,7 +65,50 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "nutritionChannel",
+                "nutrition channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // Scheduling Alarm
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        // Set the time you want the alarm to go off
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY)) // Change this to the desired hour
+        calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE)+1) // Change this to the desired minute
+
+        val intent = Intent(applicationContext, AlarmReceiver::class.java)
+        val requestCode = 123 // Use a unique value here
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                pendingIntent
+            )
+        } else {
+            alarmManager.setExact(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                pendingIntent
+            )
+        }
     }
+
 
     public override fun onStart() {
         super.onStart()
@@ -52,6 +118,9 @@ class MainActivity : AppCompatActivity() {
         }
         updateUI(currentUser)
     }
+
+
+
 
     private fun updateUI(user: FirebaseUser?) {
     }
